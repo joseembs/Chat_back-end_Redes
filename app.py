8905315055d1ba7161bf411444fa6f07538b8b61
elisrb <elisrb2004@gmail.com>
2024-07-27 17:38:17 -0300

    versão nova

--- app.py
+++ app.py
@@ -1,64 +1,103 @@
-from flask import Flask, request, jsonify
+"""
+! esse código não roda sozinho, ele é só um monte de funções
+(elas devem ser chamadas pelo servidor em algum momento)
 
-from CriaProcessos import main
-import ExemploIRC as IRC
+LEGENDA
+comentário no final da linha de código: explica a linha em que ele está
+comentário sozinho na linha:
+    # = linhas antigas que eu comentei
+    ## = coisas pra fazer depois
+        ! = precisa
+        ? = não precisa
 
-app = Flask(__name__)
+no começo do código tem alguns imports velhos de quando a gente tava usando flask (rip)
+no final do código tem um teste que eu to usando pra rodar o código sozinho
+"""
 
-def remove_email_suffix(email):
-    aux = email
-    aux.replace('@', '')
-    return aux.replace('.', '')
+import json
+import re
 
-@app.route('/api', methods=['GET'])
-def getJson():  # put application's code here
-    payload={}
+#from flask import Flask, request, jsonify
+#from CriaProcessos import main
+#import ExemploIRC as IRC
 
-    payload['pedido'] = str(request.args['pedido'])
+# app = Flask(__name__)
 
-    if('email' in request.args != None):
-        payload['email'] = str(request.args['email'])
+# @app.route('/api', methods=['GET'])
+def getJson(jsonIn:json):
+    payload = json.loads(jsonIn)
+    response = {}
 
-    if('mensagem' in request.args != None):
-        payload['mensagem'] = str(request.args['mensagem'])
+    match payload['pedido']:
 
-    if(payload['pedido'] == "cadastro"):
-        #if("@gmail.com" in payload['email']): (a gente vai validar o email???)
-        file = open("emails.txt", "a")
-        file.write(f"{payload['email']}\n")
-        file.close()
+        case "cadastro":
+            if re.search(r"\w.*@\w+\.\w+", payload['email']):
+                file = open("emails.txt", "a")
+                file.write(f"{payload['email']}\n")
+                file.close()
+                
+                file = open("chats.txt", "a")
+                file.write(f"{payload['email']}\n")
+                file.close()
 
-        aux = remove_email_suffix(payload["email"])
-        file = open(f"{aux}.txt", "a")
-        file.write(f"{payload['email']}\n")
-        file.close()
+                response['cadastrado'] = True
+
+            else:
+                response['cadastrado'] = False
+
+            ## ! cadastrar nome e local do usuário
     
-    if(payload['pedido'] == "login"):
-        file = open("emails.txt", 'r')
-        if(payload['email'] in file.read()):
-            payload['cadastrado'] = True
-        else:
-            payload['cadastrado'] = False
-        file.close()
+        case "login":
+            file = open("emails.txt", 'r')
+            if(payload['email'] in file.read()):
+                payload['cadastrado'] = True
+            else:
+                payload['cadastrado'] = False
+            file.close()
 
-    if(payload['pedido'] == "atualizar"):
-        file = open("emails.txt", 'r')
-        payload['allUsers'] = []
-        for email in file.read().split():
-            payload['allUsers'].append(email)
-        payload['allUsers'].sort()
-        file.close()
-        print(payload['allUsers'])
+        case "atualizar":
+            file = open("chats.txt", 'r')
+            payload['allUsers'] = []
+            for email in file.read().split():
+                payload['allUsers'].append(email)
+            payload['allUsers'].sort()
+            file.close()
+            # print(payload['allUsers'])
+            # print(payload)
 
-    print(payload)
+        case "criaGrupo":
+            file = open("chats.txt", "a")
+            file.write(f"{payload['mensagem']}\n") # mensagem = nome do grupo
+            file.close()
 
-    if(payload['pedido'] == "sendMsg"):
-        print("foi")
-        # print(mensagem)
-        # IRC.funciona(payload['mensagem'])
+            aux = re.sub("\W", "", payload['mensagem'])
+            file = open(f"{aux}.txt", "a")
+            file.write(f"{payload['email']}\n") # email = adm do grupo
+            file.close()
 
-    return jsonify(payload)
+        case "addGrupo":
+            aux = re.sub("\W", "", payload['mensagem'])
+            file = open(f"{aux}.txt", "a")
+            file.write(f"{payload['email']}\n") # email = novo integrante do grupo
+            file.close()
+
+        case "sendMsg":
+            # print(payload['mensagem'])
+            # IRC.funciona(payload['mensagem'])
+
+            ## ! definir grupo de destino
+
+            pass
+
+    return json.dumps(response)
+
+    ## ! resolver o que o payload precisa voltar pro servidor
 
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+    # app.run()
+
+"""
+dicionario = {"pedido":"criaGrupo", "email":"a@gmail.com", "mensagem":"urubu$abobrinha"} # teste
+print(getJson(json.dumps(dicionario)))
+"""
