b5ee2acdbe74c173aaf3bd37eacc184b310f5d49
elisrb <elisrb2004@gmail.com>
2024-07-27 20:42:35 -0300

    add getDM e sendDM

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
 """
-! esse código não roda sozinho, ele é só um monte de funções
-(elas devem ser chamadas pelo servidor em algum momento)
-
-LEGENDA
-comentário no final da linha de código: explica a linha em que ele está
-comentário sozinho na linha:
-    # = linhas antigas que eu comentei
-    ## = coisas pra fazer depois
-        ! = precisa
-        ? = não precisa
-
-no começo do código tem alguns imports velhos de quando a gente tava usando flask (rip)
-no final do código tem um teste que eu to usando pra rodar o código sozinho
+! esse código não roda sozinho
+(a função getJson deve ser chamada pelo servidor)
 """
 
 import json
 import re
 
-#from flask import Flask, request, jsonify
-#from CriaProcessos import main
-#import ExemploIRC as IRC
-
-# app = Flask(__name__)
-
-# @app.route('/api', methods=['GET'])
-
 def formata(string):
     return re.sub(r"\W", "", string)
-
 
 def getJson(jsonIn:json):
     payload = json.loads(jsonIn)
@@ -36,16 +16,19 @@ def getJson(jsonIn:json):
     match payload['pedido']:
 
         case "cadastro":
-            if re.search(r"\w.*@\w+\.\w+", payload['email']):
-                try:
+            
+            response['cadastrado'] = False
+            
+            if re.search(r"\w.*@\w+\.\w+", payload['email']): # só realiza o cadastro caso o email seja válido
+                try: # pega os usuários já cadastrados
                     file = open("users.json", 'r')
                     aux = json.load(file)
                     file.close()
 
-                except:
+                except: # caso não haja usuários
                     aux = {}
                 
-                if(payload['email'] not in aux.keys()):
+                if(payload['email'] not in aux.keys()): # evita o cadastro de um email repetido
                     aux[payload['email']] = {"nome" : payload['nome'], "local" : payload['local']}
                     file = open("users.json", 'w')
                     file.write(json.dumps(aux))
@@ -56,21 +39,19 @@ def getJson(jsonIn:json):
                     file.close()
 
                     response['cadastrado'] = True
-                else:
-                    response['cadastrado'] = False
-
-            else:
-                response['cadastrado'] = False
     
         case "login":
+            
             file = open("users.json", 'r')
-            if(payload['email'] in file.read()):
+            aux = json.load(file)
+            file.close()
+            if(payload['email'] in aux.keys()):
                 response['cadastrado'] = True
             else:
                 response['cadastrado'] = False
-            file.close()
 
         case "atualizar":
+            
             file1 = open("chats.txt", 'r')
             file2 = open("users.json", 'r')
             aux = json.load(file2)
@@ -79,32 +60,22 @@ def getJson(jsonIn:json):
             response['allUsers'] = []
             response['allGroups'] = []
 
-            for email in file1.read().split("\n"):
+            for email in file1.read().split("\n"): # lê a lista de todas as conversas
                 if(email != ""):
-                    if(email in aux.keys()):
+                    if(email in aux.keys()): # separa entre grupos e usuários
                         response['allUsers'].append(email)
                     else:
                         response['allGroups'].append(email)
             response['allUsers'].sort()
             response['allGroups'].sort()
+            
             file1.close()
 
         case "criaGrupo":
-            # payload: nome = nome do grupo, email = email do adm
-
-            """# caso dm?
-            if('nome' in payload.keys()):
-                nome = formata(payload['nome'])
-            else: 
-                nome1 = formata(payload['email']) 
-                nome2 = formata(payload['destinatario'])
-                if(nome1 < nome2):
-                    nome = nome1+nome2
-                else:
-                    nome = nome2+nome1"""
+            ## payload: nome = nome do grupo, email = email do adm
 
             nome = formata(payload['nome'])
-            response = {
+            response = { # cria json com as informações do grupo
                 "quant" : 0,
                 "members" : [payload['email']],
                 "who" : [],
@@ -120,20 +91,22 @@ def getJson(jsonIn:json):
             file.close()
 
         case "addGrupo":
-            # payload: nome = nome do grupo, email = user atual
+            ## payload: nome = nome do grupo, email = user atual
+            
             nome = formata(payload['nome'])
             file = open(f"{nome}.json", 'r')
             response = json.load(file)
             file.close()
 
-            response['members'].append(payload['email'])
+            response['members'].append(payload['email']) # adiciona um membro
 
             file = open(f"{nome}.json", 'w')
             json.dump(response, file)
             file.close()
 
         case "sendMsg":
-            # payload: nome = nome do grupo, email = user atual, mensagem = mensagem
+            ## payload: nome = nome do grupo, email = user atual, mensagem = mensagem
+            
             nome = formata(payload['nome'])
             file = open(f"{nome}.json", 'r')
             response = json.load(file)
@@ -148,12 +121,61 @@ def getJson(jsonIn:json):
             file.close()
         
         case "getHistorico":
+            
             nome = formata(payload['nome'])
             file = open(f"{nome}.json", 'r')
             response = json.load(file)
             file.close()
 
+        case "getDM":
+            ## payload: destinatario = email do destinatario, email = user atual
+
+            nome1 = formata(payload['email']) 
+            nome2 = formata(payload['destinatario'])
+            if(nome1 < nome2):
+                nome = nome1+nome2
+            else:
+                nome = nome2+nome1
+
+            try: # vê se a dm já existe
+                file = open(f"{nome}.json", 'r')
+                response = json.load(file)
+                file.close()
+            except: # cria a dm
+                response = {
+                    "quant" : 0,
+                    "members" : [payload['email'], payload['destinatario']],
+                    "who" : [],
+                    "hist" : []
+                }
+
+                file = open(f"{nome}.json", 'w')
+                json.dump(response, file)
+                file.close()
+
+        case "sendDM":
+            ## payload: destinatario = email do destinatario, email = user atual, mensagem = mensagem
+
+            nome1 = formata(payload['email']) 
+            nome2 = formata(payload['destinatario'])
+            if(nome1 < nome2):
+                nome = nome1+nome2
+            else:
+                nome = nome2+nome1            
+            
+            file = open(f"{nome}.json", 'r')
+            response = json.load(file)
+            file.close()
+
+            response['quant'] += 1
+            response['who'].append(payload['email'])
+            response['hist'].append(payload['mensagem'])
+
+            file = open(f"{nome}.json", 'w')
+            json.dump(response, file)
+            file.close()
+
     return json.dumps(response)
 
-dicionario = {"pedido":"sendMsg", "email":"a@gmail.com", "mensagem":"ola grupo", "nome":"grupo legal"} # teste
-print(getJson(json.dumps(dicionario)))
+"""dicionario = {"pedido":"sendDM", "email":"a@gmail.com", "mensagem":"ola grupo", "destinatario":"b@gmail.com"} # teste
+print(getJson(json.dumps(dicionario)))"""
