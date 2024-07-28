84183b2c38bcebaf571fd5de2944ad80891ac2e1
José Edson MBS <joseedsonmbs04@gmail.com>
2024-07-28 00:54:21 -0300

    pequenas mudanças, sendDM substituído e testes

--- app.py
+++ app.py
@@ -105,9 +105,20 @@ def getJson(jsonIn:json):
             file.close()
 
         case "sendMsg":
-            ## payload: nome = nome do grupo, email = user atual, mensagem = mensagem
-            
-            nome = formata(payload['nome'])
+            ## payload: nome = nome do chat (email ou grupo), email = user atual, mensagem = mensagem, grupo = true/false (é grupo ou não)
+
+            grupo = payload['grupo']
+
+            if(grupo):
+                nome = formata(payload['nome'])
+            else:
+                nome1 = formata(payload['email'])
+                nome2 = formata(payload['nome'])
+                if(nome1 < nome2):
+                    nome = nome1+nome2
+                else:
+                    nome = nome2+nome1
+
             file = open(f"{nome}.json", 'r')
             response = json.load(file)
             file.close()
@@ -119,8 +130,8 @@ def getJson(jsonIn:json):
             file = open(f"{nome}.json", 'w')
             json.dump(response, file)
             file.close()
-        
-        case "getHistorico":
+
+        case "getGrupo":
             
             nome = formata(payload['nome'])
             file = open(f"{nome}.json", 'r')
@@ -128,10 +139,10 @@ def getJson(jsonIn:json):
             file.close()
 
         case "getDM":
-            ## payload: destinatario = email do destinatario, email = user atual
+            ## payload: nome = email do destinatario, email = user atual
 
             nome1 = formata(payload['email']) 
-            nome2 = formata(payload['destinatario'])
+            nome2 = formata(payload['nome'])
             if(nome1 < nome2):
                 nome = nome1+nome2
             else:
@@ -144,7 +155,7 @@ def getJson(jsonIn:json):
             except: # cria a dm
                 response = {
                     "quant" : 0,
-                    "members" : [payload['email'], payload['destinatario']],
+                    "members" : [payload['email'], payload['nome']],
                     "who" : [],
                     "hist" : []
                 }
@@ -153,29 +164,36 @@ def getJson(jsonIn:json):
                 json.dump(response, file)
                 file.close()
 
-        case "sendDM":
-            ## payload: destinatario = email do destinatario, email = user atual, mensagem = mensagem
-
-            nome1 = formata(payload['email']) 
-            nome2 = formata(payload['destinatario'])
-            if(nome1 < nome2):
-                nome = nome1+nome2
-            else:
-                nome = nome2+nome1            
-            
-            file = open(f"{nome}.json", 'r')
-            response = json.load(file)
-            file.close()
-
-            response['quant'] += 1
-            response['who'].append(payload['email'])
-            response['hist'].append(payload['mensagem'])
-
-            file = open(f"{nome}.json", 'w')
-            json.dump(response, file)
-            file.close()
-
     return json.dumps(response)
 
-"""dicionario = {"pedido":"sendMsg", "email":"a@gmail.com", "mensagem":"ola grupo", "nome":"grupo legal"} # teste
-print(getJson(json.dumps(dicionario)))"""
+# dicionario = {"pedido":"cadastro", "email":"j@gmail.com", "nome":"J", "local": "Brasil"} # teste
+# print(getJson(json.dumps(dicionario)))
+# {"cadastrado": true}
+
+# dicionario = {"pedido":"login", "email":"j@gmail.com"}
+# print(getJson(json.dumps(dicionario)))
+# {"cadastrado": true}
+
+# dicionario = {"pedido":"atualizar"}
+# print(getJson(json.dumps(dicionario)))
+# {"allUsers": ["j@gmail.com"], "allGroups": []}
+
+# dicionario = {"pedido":"cadastro", "email":"o@gmail.com", "nome":"O", "local": "EUA"}
+# print(getJson(json.dumps(dicionario)))
+# {"cadastrado": true}
+
+# dicionario = {"pedido":"atualizar"}
+# print(getJson(json.dumps(dicionario)))
+# {"allUsers": ["j@gmail.com", "o@gmail.com"], "allGroups": []}
+
+# dicionario = {"pedido": "getDM", "email": "j@gmail.com", "destinatario": "o@gmail.com"}
+# print(getJson(json.dumps(dicionario)))
+# {"quant": 0, "members": ["j@gmail.com", "o@gmail.com"], "who": [], "hist": []}
+
+# dicionario = {"pedido": "sendDM", "destinatario": "o@gmail.com", "email": "j@gmail.com", "mensagem": "oi"} // antigo
+# print(getJson(json.dumps(dicionario)))
+# {"quant": 1, "members": ["j@gmail.com", "o@gmail.com"], "who": ["j@gmail.com"], "hist": ["oi"]}
+
+# dicionario = {"pedido": "sendMsg", "nome": "j@gmail.com", "email": "o@gmail.com", "mensagem": "eae", "grupo": False} // novo
+# print(getJson(json.dumps(dicionario)))
+# {"quant": 2, "members": ["j@gmail.com", "o@gmail.com"], "who": ["j@gmail.com", "o@gmail.com"], "hist": ["oi", "eae"]}
