ea025a21b577df7e94ad27f7f0741b5230afc1d2
Henrique Sales <144968260+hsaless@users.noreply.github.com>
2024-08-04 15:27:42 -0300

    update final

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def handle_client(client_socket, addr):
     while True:
         try:
             info = client_socket.recv(1024).decode("utf-8")
-            print("aqui1")
+            
 
             if not info:
                 remove_client(client_socket, addr)
@@ -20,14 +20,10 @@ def handle_client(client_socket, addr):
             buffer += info
 
             while '\n' in buffer:
-                print("aqui3")
                 message, buffer = buffer.split('\n', 1) # para detectar o fim de cada mensagem
-                print("aqui6")
                 response = app.getJson(message, client_socket)
-                print("aqui7")
                 client_socket.send((response + '\n').encode("utf-8"))
             else:
-                print("aqui4")
                 remove_client(client_socket, addr)
                 break
         except:
@@ -37,10 +33,7 @@ def handle_client(client_socket, addr):
 
 def recebe_arquivo(client_socket, filename):
     try:
-        # nome do arquivo
-        # filename = client_socket.recv(1024).decode()
-        # print(f"Recebendo arquivo: {filename}")
-        print("começa a baixar")
+        print("Começa a baixar")
         # arquivo em si
         with open(filename, 'wb') as f:
             a = 0
@@ -54,14 +47,9 @@ def recebe_arquivo(client_socket, filename):
                 f.write(data)
         print(f"Arquivo {filename} recebido com sucesso.")
 
-        """""
-        # Enviar o arquivo de volta
-        with open(filename, 'rb') as f:
-            client_socket.sendfile(f)
-        print(f"Arquivo {filename} enviado de volta com sucesso.")
-        """""
+    
     except:
-        print(f"deu erro :/")
+        print(f"Ocorreu um erro")
 
 def envia_arquivo(client_socket, filename):
     try:
@@ -69,7 +57,7 @@ def envia_arquivo(client_socket, filename):
             client_socket.sendfile(f)
         print(f"Arquivo {filename} enviado de volta com sucesso.")
     except:
-        print(f"deu erro :(")
+        print(f"Ocorreu um erro")
 
 def broadcast(message, sender_addr):
     for client_socket, addr in clients.items():
